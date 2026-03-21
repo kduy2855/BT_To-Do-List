291febc2c2d406c82ada43121b2fcae26b9e200f
KhuongDuy <vokhuongduy2805@gmail.com>
2026-03-21 23:57:33 +0700

    Add files via upload

---
+++ core/database.py
@@ -0,0 +1,13 @@
+from collections.abc import Generator
+from sqlmodel import Session, create_engine
+from core.config import settings
+
+engine = create_engine(
+    settings.database_url,
+    connect_args={"check_same_thread": False} if settings.database_url.startswith("sqlite") else {},
+    echo=settings.debug,
+)
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
