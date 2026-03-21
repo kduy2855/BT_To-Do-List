d3b205f59a68c60d991c98f84ca84efe0cec0f21
KhuongDuy <vokhuongduy2805@gmail.com>
2026-03-21 23:51:19 +0700

    Add files via upload

---
+++ schemas/todo.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, ConfigDict, constr
+from typing import List, Optional
+from datetime import datetime
+
+class ToDoCreate(BaseModel):
+    title: constr(min_length=3, max_length=100)
+    description: Optional[str] = None
+    is_done: bool = False
+
+class ToDoUpdate(BaseModel):
+    title: Optional[constr(min_length=3, max_length=100)] = None
+    description: Optional[str] = None
+    is_done: Optional[bool] = None
+
+class ToDo(ToDoCreate):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+class ToDoListResponse(BaseModel):
+    items: List[ToDo]
+    total: int
+    limit: int
+    offset: int
