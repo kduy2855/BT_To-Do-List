a2c49ae7cd463b8122366c88c67905525076ee70
KhuongDuy <vokhuongduy2805@gmail.com>
2026-03-22 11:37:16 +0700

    Add auth router to FastAPI application

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from core.config import settings
 from routers.health import router as health_router
 from routers.todo import router as todo_router
+from routers.auth import router as auth_router
 
 app = FastAPI(
     title=settings.app_name,
@@ -10,4 +11,5 @@ app = FastAPI(
 )
 
 app.include_router(health_router, prefix=settings.api_prefix)
+app.include_router(auth_router, prefix=settings.api_prefix)
 app.include_router(todo_router, prefix=settings.api_prefix)
